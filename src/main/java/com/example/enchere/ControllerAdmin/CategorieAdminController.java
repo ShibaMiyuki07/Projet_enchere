3051b2a9f269841d2933dd0f68e50f06d59c06bf
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-01-17 11:07:27 +0300

    Insertion Categorie

--- src/main/java/com/example/enchere/ControllerAdmin/CategorieAdminController.java
+++ src/main/java/com/example/enchere/ControllerAdmin/CategorieAdminController.java
@@ -3,17 +3,28 @@ package com.example.enchere.ControllerAdmin;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.enchere.ModelAdmin.Categorie;
 
 @RestController
 @RequestMapping("/CategorieAdmin")
 public class CategorieAdminController {
 	
 	@GetMapping
-	public ArrayList<CategorieAdminController> selectAll() throws Exception
+	public ArrayList<Categorie> selectAll() throws Exception
 	{
-		ArrayList<CategorieAdminController> liste = null;
+		ArrayList<Categorie> liste = null;
 		return liste;
+	}
+	
+	@PostMapping
+	public boolean insertion(@RequestBody Categorie categorie) throws Exception
+	{
+		boolean cat = new Categorie().insert(categorie);
+		return cat;
 	}
 }
--- src/main/java/com/example/enchere/ModelAdmin/Categorie.java
+++ src/main/java/com/example/enchere/ModelAdmin/Categorie.java
@@ -1,5 +1,11 @@
 package com.example.enchere.ModelAdmin;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.example.enchere.Base.Connexion;
+
 public class Categorie {
 	private int idcategorie;
 	private String categorie;
@@ -23,5 +29,28 @@ public class Categorie {
 		this.dureeEnchereCategorie = dureeEnchereCategorie;
 	}
 	
-	
+	public boolean insert(Categorie categorie) throws Exception
+	{
+		String requete ="insert into categorie values(default,'"+categorie.getCategorie()+"','"+categorie.getDureeEnchereCategorie()+"')";
+		Connection connex = null;
+		Statement state = null;
+		boolean retour = false;
+		try
+		{
+			connex = Connexion.setConnect();
+			state = connex.createStatement();
+			state.execute(requete);
+			retour = true;
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
+			connex.close();
+			state.close();
+		}
+		return retour;
+	}
 }
