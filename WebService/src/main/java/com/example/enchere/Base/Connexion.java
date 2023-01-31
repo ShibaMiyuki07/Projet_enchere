14461c521f9185b9f938a1aba6b80cdd17435809
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-01-31 11:33:39 +0300

    Update Connexion.java

--- WebService/src/main/java/com/example/enchere/Base/Connexion.java
+++ WebService/src/main/java/com/example/enchere/Base/Connexion.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Connexion {
-	private static String url = "jdbc:postgresql://containers-us-west-26.railway.app:7589/railways";
+	private static String url = "jdbc:postgresql://containers-us-west-26.railway.app:7589/railway";
     private static String user="postgres";
     private static String passwd = "FAkbci0tk1M81H0bOl4Q";
 	private  Connection connect;
