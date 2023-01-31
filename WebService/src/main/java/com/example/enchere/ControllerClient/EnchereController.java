cf0836fa106d7549d73ac7c805ee10de45d65710
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-01-31 11:45:23 +0300

    Add cors

--- WebService/src/main/java/com/example/enchere/ControllerClient/EnchereController.java
+++ WebService/src/main/java/com/example/enchere/ControllerClient/EnchereController.java
@@ -2,6 +2,7 @@ package com.example.enchere.ControllerClient;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.enchere.ModelClient.Enchere;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/Enchere")
 public class EnchereController {
 	
